869b63b44423f9110c4a726be45c74a7cf2b0620
Zachary Miller <2.4.8.16.32.sixtyfour@gmail.com>
2017-04-16 20:29:05 -0600

    Updated server config

--- app/Server.php
+++ app/Server.php
@@ -8,13 +8,12 @@
 
 
 define('DOMAIN', 'localhost'); 
-define('ROOT_PATH', $_SERVER['ROOT_DIR'].'watchmefarm/');
+define('ROOT_PATH', $_SERVER['ROOT_DIR'].'IrrigationScheduler/');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'ima_user');
-define('DB_PASSWD', 'fotbaltym9'); 
+define('DB_USER', 'user');
+define('DB_PASSWD', 'password'); 
 define('DB_NAME', 'irrigation');
-define('CACHE_DIR', ROOT_PATH . "cache");
+define('CACHE_DIR', name . "cache");
 define('AUTOLOAD_CACHE_PATH', CACHE_DIR . "/classpaths.cache");
 
 define('PYTHON', "C:\Python27\python.exe");
-define('RMQSEND', "C:\Users\zmiller\PycharmProjects\Arduino\RMQSend.py");
