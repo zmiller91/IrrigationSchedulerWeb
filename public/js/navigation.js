7ec0c0a3fb0568d43dd27c5d85a51decb2506dbf
Zachary Miller <2.4.8.16.32.sixtyfour@gmail.com>
2017-04-25 21:40:12 -0600

    Content body takes up full window.

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,7 +1,8 @@
 define([], function() {
     return {
         init: function(app) {
-            app.controller("NavigationCtrl", function ($scope, User) {
+            app.controller("NavigationCtrl", function ($scope, $window ,User) {
+                $scope.window = {height: $window.innerHeight};
                 $scope.collapsed = true;
                 $scope.path = "";
 
@@ -16,7 +17,7 @@ define([], function() {
                 
                 $scope.getNavClass = function() {
                     return $scope.collapsed ? 'nav-sm' : 'nav-md';
-                }
+                };
                 
                 $scope.getTabClass = function(activeRoute) {
                     if(activeRoute !== $scope.path) {
@@ -24,10 +25,27 @@ define([], function() {
                     }
                     
                     return $scope.collapsed ? 'active-sm' : 'active';
-                }
+                };
 
                 $scope.$on('$routeChangeStart', function(event, next, current) { 
                     $scope.path = next.originalPath;
+                });
+                
+                // Watch for screen height
+                var w = angular.element($window);
+                $scope.$watch(
+                  function () {
+                    return $window.innerHeight;
+                  },
+                  function (value) {
+                    $scope.window = {height: value};
+                    console.log($scope.window);
+                  },
+                  true
+                );
+
+                w.bind('resize', function(){
+                  $scope.$apply();
                 });
             });
         }
